19c4be2b8cb82cc252128cf9f86f04ce18907a43
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2026-09-16 16:54:51 -0400

    Created file utents to create the routes for the utents' datas

--- routes/utents.js
+++ routes/utents.js
@@ -1,1 +1,14 @@
 // routes per prendere i dati degli utenti
+
+
+//SOLO ROUTES NON LOGICA
+
+const express = require('express');
+const router = express.Router()
+const utentiControllers = require('../controllers/utentsControllers.js')
+
+
+router.get('/', utentiControllers.getAllUtents);
+//getAllUtents metodo da creare in utentsController per prendere i dati degli utenti
+
+module.exports = router
